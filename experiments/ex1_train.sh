bac9608679654783fdf6cb123203e0b5188abad4
mickshen <mickshen7558@gmail.com>
2020-10-14 17:37:03 -0400

    a

--- experiments/ex1_train.sh
+++ experiments/ex1_train.sh
@@ -10,7 +10,7 @@ PY3="nice -n 10 python"
 # ModelNet categories are given in './sampledata/modelnet40_half1.txt' (as examaple)
 ${PY3} train_classifier.py \
  -o /data/PointNet_results/ex1_classifier_0915 \
- -i /data/work/pointnet/ModelNet40 \
+ -i /data/ModelNet40/modelnet40_ply_hdf5_2048/ \
  -c ./sampledata/modelnet40_half1.txt \
  -l /data/PointNet_results/ex1_classifier_0915.log \
  --device cuda:1
@@ -21,7 +21,7 @@ ${PY3} train_classifier.py \
 # train PointNet-LK. fine-tune the PointNet feature for classification (the above file).
 ${PY3} train_pointlk.py \
  -o /data/PointNet_results/ex1_pointlk_0915 \
- -i /home/yasuhiro/work/pointnet/ModelNet40 \
+ -i /data/ModelNet40/modelnet40_ply_hdf5_2048/ \
  -c ./sampledata/modelnet40_half1.txt \
  -l /data/PointNet_results/ex1_pointlk_0915.log \
  --transfer-from /data/PointNet_results/ex1_classifier_0915_feat_best.pth \
